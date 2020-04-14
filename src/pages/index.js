e3582025bec463c6a5e243a0e775b7237958c7af
Mike Mikucki <mikem@Mikes-MBP.broadband>
2020-04-14 22:31:03 +0100

    fix header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
-import Header from '../components/Header'
+import Header from '../components/header'
 import About from '../components/About'
 import Offer from '../components/Offer'
 import FollowUs from '../components/FollowUs'
